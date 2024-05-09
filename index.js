73d58f052d9e2ff59e66750ca7ff4b1065dc6021
linhanhhihihi <jhonfelipapun0suy@hotmail.com>
2024-05-10 02:18:18 +0700

    test task

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-function maxProfit(prices) {
-  let minPrice = Infinity;
-  let maxProfit = 0;
-  for (const price of prices) {
-    minPrice = Math.min(minPrice, price);
-    maxProfit = Math.max(maxProfit, price - minPrice);
+function maximalSquare(matrix) {
+  if (matrix.length === 0) return 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const dp = Array.from(Array(rows + 1), () => Array(cols + 1).fill(0));
+  let maxSide = 0;
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= cols; j++) {
+      if (matrix[i - 1][j - 1] === "1") {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
+        maxSide = Math.max(maxSide, dp[i][j]);
+      }
+    }
   }
-  return maxProfit;
+  return maxSide * maxSide;
 }
