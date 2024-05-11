0fbe45ec01758984e10f714290ef83f5b149165e
linhanhhihihi <jhonfelipapun0suy@hotmail.com>
2024-05-12 00:31:34 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-function deserialize(data) {
-  if (!data.length) return null;
-  const root = new TreeNode(data.shift());
-  const queue = [root];
-  while (queue.length) {
-    const node = queue.shift();
-    const leftVal = data.shift();
-    if (leftVal !== undefined) {
-      node.left = new TreeNode(leftVal);
-      queue.push(node.left);
-    }
-    const rightVal = data.shift();
-    if (rightVal !== undefined) {
-      node.right = new TreeNode(rightVal);
-      queue.push(node.right);
-    }
-  }
-  return root;
+function squareDigits(num) {
+  return parseInt(
+    num
+      .toString()
+      .split("")
+      .map((digit) => digit ** 2)
+      .join(""),
+  );
 }
